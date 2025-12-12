4de773a60f6e9c1d3dd0e041fd1a165e8b7b69d4
Thrasher66099 <kphillips48858@gmail.com>
2025-12-12 11:49:37 -0500

    Initial Commit

---
+++ StrongAI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StrongAI
+//
+//  Created by Kaleb Phillips  on 12/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
